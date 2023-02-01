93a09c8e30459942871b711469d9d71f85cdd253
BOhyun <tjqhgus1011@gmail.com>
2023-02-01 22:33:19 +0900

    PortFolio

---
+++ portfolio/포트폴리오/JDBCTest/JDBCTest/src/main/java/bohyun/JDBCTest/JdbcTestApplication.java
@@ -0,0 +1,13 @@
+package bohyun.JDBCTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcTestApplication.class, args);
+	}
+
+}
---
+++ portfolio/포트폴리오/JDBCTest/JDBCTest/src/test/java/bohyun/JDBCTest/JdbcTestApplicationTests.java
@@ -0,0 +1,13 @@
+package bohyun.JDBCTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
